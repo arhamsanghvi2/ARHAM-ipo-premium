22e4081a79eca0828c75c8021f59bbb46d1aa415
arhamsanghvi2 <arhamsanghvi2@gmail.com>
2026-09-10 17:53:40 +0530

    3thchange

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,14 @@
 const nextConfig = {
   serverExternalPackages: [
     'puppeteer',
+    'puppeteer-core',
     'puppeteer-extra',
     'puppeteer-extra-plugin-stealth',
+    'puppeteer-extra-plugin',
+    '@sparticuz/chromium',
+    'clone-deep',
+    'merge-deep',
+    'is-plain-object',
     'better-sqlite3',
     'jsonwebtoken',
   ],
